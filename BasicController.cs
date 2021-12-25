654f8fd3ce2d0d7403add4ea3b9599c7d52a09ce
VladislavTerentev <96658755+VladislavTerentev@users.noreply.github.com>
2021-12-25 22:14:16 +0300

    Add files via upload

---
+++ BasicController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update() {
+      Debug.Log ("Horizontal Input = "+Input.GetAxis ("Horizontal"));
+
+    }
+}
